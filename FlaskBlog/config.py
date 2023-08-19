65ac0cbbd5b6ba8249c453ce850847860f5de37a
B-toxin <142661535+B-toxin@users.noreply.github.com>
2023-08-19 10:46:44 +0100

    Update config.py

--- FlaskBlog/config.py
+++ FlaskBlog/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = 'e71121f8359c7c241f56e489f91f32d7'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dblog_liat_user:mdXZHiMAb0lKXAj1mKENxQzf6XrFgByZ@dpg-cjfnuiocfp5c73b2f65g-a.ohio-postgres.render.com/dblog_liat'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dblog_5d0l_user:jEwPqP04nWhUTPh54eiIBNGTVuojUgY5@dpg-cjg8snb37aks73b73np0-a.ohio-postgres.render.com/dblog_5d0l'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
